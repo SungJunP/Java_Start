1c9806e265558635d82c88eb6c27977a603047e0
SungJun <sungjun95623@gmail.com>
2023-11-29 19:28:24 +0900

    변수 끝

---
+++ src/variable/VarEx3.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class VarEx3 {
+
+    public static void main(String[] args) {
+
+        long a = 100000000L;
+        System.out.println(a);
+
+        boolean b = true;
+        System.out.println(b);
+    }
+}
