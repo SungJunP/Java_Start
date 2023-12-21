4e1960ff29f8e39b4dd80fb2b7b4c9c5b06d6be0
SungJun <sungjun95623@gmail.com>
2023-12-21 19:25:07 +0900

    배열

---
+++ src/array/ArrayDi0.java
@@ -0,0 +1,28 @@
+package array;
+
+public class ArrayDi0 {
+
+    public static void main(String[] args) {
+        // 2 * 3 2차원 배열을 만든다.
+        int[][] arr = new int[2][3]; // 행 2 , 열 3
+
+        arr[0][0] = 1; // 0행, 0열
+        arr[0][1] = 2; // 0행, 1열
+        arr[0][2] = 3; // 0행, 2열
+        arr[1][0] = 4; // 1행, 0열
+        arr[1][1] = 5; // 1행, 1열
+        arr[1][2] = 6; // 1행, 2열
+
+        // 0 행 출력
+        System.out.print(arr[0][0] + " "); // 0 열 출력
+        System.out.print(arr[0][1] + " "); // 0 열 출력
+        System.out.print(arr[0][2] + " "); // 0 열 출력
+
+        System.out.println(); // 한 행이 끝나면 라인을 변경한다.
+
+        // 1 행 출력
+        System.out.print(arr[1][0] + " "); // 0 행 출력
+        System.out.print(arr[1][1] + " "); // 1 행 출력
+        System.out.print(arr[1][2] + " "); // 2 행 출력
+    }
+}
