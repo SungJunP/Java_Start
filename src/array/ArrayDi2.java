1009b8f4be497e400ae6f9e3f8d18798dd8b4d14
SungJun <sungjun95623@gmail.com>
2023-12-23 00:15:33 +0900

    배열

--- src/array/ArrayDi2.java
+++ src/array/ArrayDi2.java
@@ -4,7 +4,7 @@ public class ArrayDi2 {
 
     public static void main(String[] args) {
         // 2 * 3 2차원 배열을 만든다.
-        int[][] arr = new int[2][3]; // 행 2 , 열 3
+        int[][] arr = new int[2][3];  // 행 2 , 열 3
 
         arr[0][0] = 1; // 0행, 0열
         arr[0][1] = 2; // 0행, 1열
